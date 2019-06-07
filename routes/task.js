7af0f82240030cb84540a9afa13ec0b49cd8f19a
Jyolainnie Daniel <jyolainnie.daniel@hyuna.bb>
2019-06-07 12:05:09 -0400

    Working on mongodb

--- routes/task.js
+++ routes/task.js
@@ -1,88 +1,116 @@
+const mongoose = require('mongoose');
 const Joi = require('joi');
 const express = require('express');
 const uniqid = require('uniqid');
 const router = express.Router();
 
-const todosArray = []
+//define a schema for tasks
+const taskSchema = new mongoose.Schema({
+    Title: {
+        type: String, 
+        required: true,
+        minlength: 5,
+        maxlength:50
+    },
+    Description:{
+        type: String,
+        required: true,
+        minlength: 10,
+        maxlength: 255
+    },
+    Status: {
+       type: String,
+       default: 'To Do'
+    },
+    DateCreated: {
+        type: Date, 
+        default: Date.now
+    },
+    DateComplete: {
+        type: Date
+    }
+});
+
+//create model from task schema
+const Task = mongoose.model('Task', taskSchema);
+
+
+//response to show all tasktodosArray
+router.get('/showAll', async(req, res) =>
+{
+    const tasks = await Task.find().sort('DateCreated');
+    res.send(tasks);
+});
 
 //response to add request
-router.post('/add', (req, res) =>
+router.post('/add', async(req, res) =>
 {
     //validating input
-    const schema = {
-        Title: Joi.string().min(5).required(),          //title must be string and at least 3 characters
-        Description: Joi.string().min(10).required()
-    }
-    
-    const result = Joi.validate(req.body, schema);
-    
+    const result = validateTask (req.body);  
     if(result.error) return res.status(400).send(result.error.details[0].message);          //Bad Request
     
-    let date = new Date();
-    
-    let task = {
-        ID: uniqid(),
+    //create document
+    let task = new Task({
         Title: req.body.Title,
-        Description: req.body.Description,
-        Status: 'To Do',
-        DateCreated: date.getMonth()+1 + '/'+ date.getDate() + '/' + date.getFullYear() + ' -- ' + date.getHours() +':'+String(date.getMinutes()).padStart(2, '0'),
-        DateCompleted: 'No'  
-    };
+        Description: req.body.Description
+    });
 
-    //push new task object to todoArray
-    todosArray.push(task);
-
+    //save document to database
+    task = await task.save();
     res.send(task);
 });
 
 
-//response to delete a task request
-router.delete('/delete', (req, res) =>
+//response to edit request
+router.put('/edit/:id', async(req, res) =>
 {  
-    //checks is task exists
-    const task = todosArray.find(t => t.ID === parseInt(req.body.ID));
+    //validating input
+    const result = validateTask (req.body);  
+    if(result.error) return res.status(400).send(result.error.details[0].message);          //Bad Request
+
+    //checks if task exists and updates it
+    const task = await Task.findByIdAndUpdate(req.params.ID,
+        { 
+        Title: req.body.Title,
+        Description: req.body.Description,
+        Status: req.body.Status,
+        DateComplete: req.body.DateComplete
+    }, {new: true});
+    if(!task) res.status(404).send("This task doesn't exist");   //404: Object not found
+
+    //send task item 
+    res.send(task);
+});
+
+//response to delete a task request
+router.delete('/delete/:id', async(req, res) =>
+{  
+    //checks is task exists and deletes it
+    const task = await Task.findByIdAndRemove(req.params.ID);
     if(!task) res.status(404).send("This task doesn't exist");   //404: Object not found
     
-    //find index of task in aaray
-    const index = todosArray.indexOf(task);
-    
-    //remove all elements from todoArray
-    todosArray.splice(index,1);
-    
-    res.send('Task has been deleted!');
+    res.send(task);
 });
 
 
 //response to delete all tasks request
-router.delete('/deleteAll', (req, res) =>
+router.delete('/deleteAll/:id', async(req, res) =>
 {
-    //remove all elements from todoArray
-    todosArray.splice(0,todosArray.length);
-
+    //checks is task exists and deletes it
+    const task = await Task.delete({});
     res.send('Task(s) will be deleted!');
 });
 
+//used to validate requests
+function validateTask (body){
+    //validating input
+    const schema = {
+    Title: Joi.string().min(5).max(50).required(),       
+    Description: Joi.string().min(10).max(225).required()
+    }
 
-//response to delete request
-router.post('/edit', (req, res) =>
-{  
-    //checks is task exists
-    const task = todosArray.find(t => t.ID === req.body.ID);
-    if(!task) res.status(404).send("This task doesn't exist");      //404: Object not found
-    
-    //find index of task in aaray
-    const index = todosArray.indexOf(task);
-    
-    //send task item 
-    res.send(todosArray[index]);
-});
-
-
-//response to show all task
-router.get('/showAll', (req, res) =>
-{
-    res.send(todosArray);
-});
+    return Joi.validate(body, schema);
+}
 
 
 module.exports = router;
